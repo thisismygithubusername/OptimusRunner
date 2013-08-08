bda42a7433299b4fa20bce05b50bcc408c12a62a
thisismygithubusername <jorge.salcedo@MB410.SLO.MBO.AD>
2013-08-08 12:44:06 -0700

    Remove bullshit

--- Twitter/Twitter.Tests/Twitter.Tests/Config/AccountSettings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Twitter.Tests.Config
-{
-    public class AccountSettings
-    {
-        public const string UserName = "jsal91@gmail.com";
-        public const string Password = "Na9ch1to";
-    }
-}
